0a35081306b318c5505585d7501012cced6baa65
Randall Lee Spence <randall@analogueweb.com>
2025-08-13 15:56:09 -0700

    Add minimum zoom level.

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,28 @@
 ﻿/* global document, window*/
 let zoomFactor = 1;
 const
+    // Level dimensions in tiles
     LEVEL_HEIGHT = 10,
     LEVEL_WIDTH = 20,
+    MIN_ZOOM = 1,
     // TILE_SIZE is the size of each tile in pixels
     TILE_SIZE = 16,
     canvas = document.getElementById('game'),
+    // Set the scaling factor for the game canvas
     setZoomFactor = function () {
-        zoomFactor = Math.min(
-            Math.floor(window.innerWidth / (LEVEL_WIDTH * TILE_SIZE)),
+        const
+            horizontalZoom = Math.floor(window.innerWidth / (LEVEL_WIDTH * TILE_SIZE)),
             // eslint-disable-next-line no-extra-parens
-            Math.floor((window.innerHeight * 0.8) / (LEVEL_HEIGHT * TILE_SIZE))
-        );
+            verticalZoom = Math.floor((window.innerHeight * 0.8) / (LEVEL_HEIGHT * TILE_SIZE));
+
+        // Calculate the zoom factor based on the window size and level dimensions
+        zoomFactor = Math.max(Math.min(horizontalZoom, verticalZoom), MIN_ZOOM);
+        // Set the canvas dimensions based on the zoom factor
         canvas.height = LEVEL_HEIGHT * TILE_SIZE * zoomFactor;
         canvas.width = LEVEL_WIDTH * TILE_SIZE * zoomFactor;
     };
 
+// Set the initial zoom factor and canvas dimensions
 setZoomFactor();
 window.addEventListener('resize', () => {
     setZoomFactor();
